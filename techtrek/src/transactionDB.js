bff8eef436c945eca7bc279a3b6c84c138632f24
vincentprofile <vincentccs93@gmail.com>
2021-03-05 14:34:42 +0800

    removed database file

--- techtrek/src/transactionDB.js
+++ techtrek/src/transactionDB.js
@@ -1,51 +1,70 @@
 import React, { Component } from 'react'
-import sqlite3 from 'sqlite3'
-import { open } from 'sqlite'
 
-class transactionDB extends Component {
-    createTransactionTable = () => {
-        open({
-            filename: './src/transactionData.db',
-            driver: sqlite3.Database
-        }).then((db) => {
-            db.exec('CREATE TABLE Username (col TEXT), Transaction_Id (col TEXT), Time_Of_Transaction (col TEXT), Buy_Or_Sell (col TEXT), Amount (col TEXT)')
-        })
+class TransactionTable extends Component {
+    state = {
+        transactionData : [],
     }
-    addTransactionToDB = () => {
-        open({
-            filename: './src/transactionData.db',
-            driver: sqlite3.Database
-        }).then((db) => {
-        })
-    }
-    queryTransactionData = () => {
-        open({
-            filename: './src/transactionData.db',
-            driver: sqlite3.Database
-        }).then((db) => {
-        })
-    }
-}
 
-const TableHeader = () => {
-    return (
-        <thread>
-            <tr>
-                <th>Name</th>
-                <th>Job</th>
-                <th>Remove</th>
-            </tr>
-        </thread>
-
-    )
-}
-
-const Table = () => {
+    tableHeader = () => {
         return (
-            <table>
-                <TableHeader />
-            </table>
+            <thread>
+                <tr>
+                    <th>Transaction ID</th>
+                    <th>Time of Transaction</th>
+                    <th>Buy/Sell</th>
+                    <th>Asset Symbol</th>
+                    <th>Asset Amount </th>
+                    <th>Asset Price</th>
+                    <th>Cash Amount</th>
+                </tr>
+            </thread>
+    
         )
+    }
+    tableBody = () => {
+        return (
+            <tr>
+                <td></td>
+            </tr>
+        )
+    }
+    retrieveTransactionData() {
+        axios.post('https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/transactions/view', { accountKey: '9316ce4f-560c-4b02-bb5f-cc1ae9bab7ed'}, {
+          headers: {
+            'x-api-key': 'wvn4vaj9S24aI8OiWsddf5B1h5z0EjKQ41GnONBy'
+          }
+        }).then(response => {
+            this.setState({
+    
+            })
+            console.log(response.data.transactionId);
+    
+        }).catch(error => {
+          if (error.response.status === 400) setError(error.response.data.message);
+          else setError("Something went wrong. Please try again later.");
+        });
+    }
+    Table = () => {
+            return (
+                <div>
+                <table>
+                    <TableHeader />
+                </table>
+                <button onClick={(retrieveTransactionData)}> Retrieve Transaction Data</button>
+                </div>
+            )
+    }
+    render() {
+        const { transactionData } = this.state; 
+        return (
+            <div className="Container">
+                <Table />
+            </div>
+
+        )
+    }
 }
 
-export default Table
+
+
+export default TransactionTable
