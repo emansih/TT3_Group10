aace826a3d4b6667ad1387c0c53c12db20b5dab6
Asert94 <hubertangelo123@gmail.com>
2021-03-05 15:18:10 +0800

    added dynamic version
    requires latest version of react and react-dom

---
+++ techtrek/src/CurPrice.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import axios from 'axios'
+import './CurPrice.css'
+
+class App extends React.Component {
+
+    constructor() {
+        super()
+	    this.state = {
+            prices: {}
+        }
+        axios.post('https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/pricing/current',{}, {headers: {'x-api-key': 'wvn4vaj9S24aI8OiWsddf5B1h5z0EjKQ41GnONBy'}})
+        .then((response) => {
+            console.log(response)
+            this.setState({ prices: response.data })
+            console.log(this.state.prices)
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+    }
+
+    render() {
+
+        return (
+            <div className="cur-price-list">
+                <h1 className="">Current TTK Price</h1>
+                <h2 className="cur-price-item">Time:{this.state.prices.timestamp}</h2>
+                <p className="cur-price-item">{this.state.prices.price} {this.state.prices.assetSymbol}</p>
+            </div>
+        )
+    }
+}
+
+
+export default CurPrice
---
+++ techtrek/src/CurPriceDynamic.js
@@ -0,0 +1,39 @@
+// --------- dependencies ---------
+// react-dom v17.0.1
+// react v17.0.1
+
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import './CurPrice.css'
+
+const CUR_REQ_INTERVAL = 10000 //Every 10 seconds
+
+function CurPrice() {
+    const [price, setPrice] = useState({})
+
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            axios.post('https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/pricing/current',{}, {headers: {'x-api-key': 'wvn4vaj9S24aI8OiWsddf5B1h5z0EjKQ41GnONBy'}})
+        .then(res => {
+            console.log(res)
+            setPrice(res.data)
+            console.log(price)
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+    }, CUR_REQ_INTERVAL)
+
+        return () => clearInterval(interval)}, [])
+
+    return (
+            <div className="cur-price-list">
+                <h1><b>Current TTK Price</b></h1>
+                <h2 className="cur-price-item">Time:{price.timestamp}</h2>
+                <p className="cur-price-item">{price.price} {price.assetSymbol}</p>
+            </div>
+            )
+}
+
+export default CurPrice
