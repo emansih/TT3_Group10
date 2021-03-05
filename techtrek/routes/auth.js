1c2625f19c0a57d39d83cd1d64ca07d5ae87e940
Sean <seanlewtengsiong@gmail.com>
2021-03-05 11:31:18 +0800

    add login validation

--- techtrek/routes/auth.js
+++ techtrek/routes/auth.js
@@ -1,6 +1,16 @@
 const router = require('express').Router();
+const Joi = require('@hapi/joi');
+const server = "https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek"
 
-router.post('/register', (req, res) => {
+dotenv.config();
+
+const loginValidation = (data) => {
+    const schema = Joi.object({
+        username: Joi.string().required(),
+        password: Joi.string().required()
+    })
+}
+router.post(server + '/login', (req, res) => {
     res.send('Register');
 });
 
