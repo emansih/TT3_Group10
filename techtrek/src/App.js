dcdacf2466e6429c00a5329c35b6790367a5bb97
emansih <danielquah148@yahoo.com>
2021-03-05 16:20:47 +1100

    Small letter dashboard

--- techtrek/src/App.js
+++ techtrek/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
 
 import Login from './Login';
-import Dashboard from './Dashboard';
+import Dashboard from './dashboard';
 import Home from './Home';
 
 function App() {
