acef94cc54cd3ebff7a185d8ad4a67973ee2659c
emansih <danielquah148@yahoo.com>
2021-03-05 18:56:36 +1100

    Fix buying and selling

--- techtrek/src/App.js
+++ techtrek/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import logo from './logo.svg';
 import './App.css';
 
@@ -18,6 +19,55 @@ function App() {
           Learn React
         </a>
       </header>
+=======
+// Done by: Lucas  5 March 2021
+
+import './App.css';
+import React, { useState } from "react"
+
+function App() {
+
+  const [fqty, setQty] = useState("")
+
+  const handleChange = e => {
+    setQty(e.target.value)
+  }
+
+  async function Buy() {
+      const requestOptions = {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json', 'x-api-key': 'wvn4vaj9S24aI8OiWsddf5B1h5z0EjKQ41GnONBy' },
+          // TODO: Remove hardcoded account key
+          body: JSON.stringify({ accountKey: '9316ce4f-560c-4b02-bb5f-cc1ae9bab7ed', 'orderType': 'BUY', 'assetAmount': fqty })
+      };
+      const json = await fetch('https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/transactions/add', requestOptions);
+  }
+
+  async function Sell() {
+
+
+      const requestOptions = {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json', 'x-api-key': 'wvn4vaj9S24aI8OiWsddf5B1h5z0EjKQ41GnONBy' },
+          // TODO: Remove hardcoded account key
+          body: JSON.stringify({ accountKey: '9316ce4f-560c-4b02-bb5f-cc1ae9bab7ed', 'orderType': 'SELL', 'assetAmount': fqty })
+      };
+      const response = await fetch('https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/transactions/add', requestOptions);
+  }
+
+  return (
+    <div className="App">
+      <p class="App-text">Enter Amount:</p>
+      <form>
+        <input type="number" value={fqty} onChange={handleChange}/>
+      </form>
+      <p> </p>
+      <p> Transaction type: </p>
+      <button disabled ={!fqty} className="buyButton" onClick={Buy} value="BUY"> Buy </button>
+      <button disabled ={!fqty} className="sellButton" onClick={Sell} value ="SELL"> Sell </button>
+      <h5>Quantity chosen: {fqty}</h5>
+      <p> </p>
+>>>>>>> Stashed changes
     </div>
   );
 }
