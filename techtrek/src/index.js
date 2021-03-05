74bd4f327bcf89dd5d03335eabc128bc172864bd
Sean <seanlewtengsiong@gmail.com>
2021-03-05 15:54:44 +0800

    resolve merge conflict 3

--- techtrek/src/index.js
+++ techtrek/src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-<<<<<<< HEAD
-ReactDOM.render(<App />, document.getElementById('root'));
-=======
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    
-    
+
+
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -18,5 +15,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
->>>>>>> a70f42b5a8d3927b996e686eb364ab25d8188988
+
